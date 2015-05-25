aecabb553ce113c18f8322c6630871da4f3d90fc
visannicoleta <visan.nicoleta@ymail.com>
2015-05-25 20:07:40 +0300

    Proiect CTS cu 3 Design Patterns finalizate

---
+++ OnlineShop/src/DesignPatterns/MementoCosCumparaturi.java
@@ -0,0 +1,15 @@
+package DesignPatterns;
+
+import java.util.ArrayList;
+
+import clase.Produs;
+
+public class MementoCosCumparaturi {
+	
+	public ArrayList<Produs> cosCumparaturi;
+	
+	public MementoCosCumparaturi(){
+		cosCumparaturi = new ArrayList<>();
+	}
+	
+}
